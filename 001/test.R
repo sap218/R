1bfdcad1437e48b656210e2f2aa5ae484a8a7c40
Samantha Pendleton <samanfapendle@outlook.com>
2017-10-07 18:07:40 +0100

    Added first R Script

---
+++ 001/test.R
@@ -0,0 +1,24 @@
+name <- "Sam"
+age <- 21
+print(name)
+print(age)
+
+print("hello world!")
+
+##
+
+v1 <- 3
+print(v1)
+
+v2 <- print(2.9 + 4.6)
+print(v2)
+
+v3 <- print(v1 + v2)
+print(v3)
+
+##
+
+print(v1 * v2)
+print(v2 / v1)
+
+##
