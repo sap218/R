333106d38e15b950708acb407f4166175bdd4b98
Samantha Pendleton <samanfapendle@outlook.com>
2017-11-04 16:01:55 +0000

    question 2 done

--- mam5120/assessed_prac_1.R
+++ mam5120/assessed_prac_1.R
@@ -4,32 +4,20 @@
 # length of leg bones in chimpanzees - standard deviation is sqrt(variance)
 m = 28 # average normal distribution
 v = 16 # variance
-
-# I
-# probability > 31.5cm
+# I - probability > 31.5cm
 pnorm(31.5, mean = m, sd = sqrt(v), lower.tail = FALSE)
-
-# II
-# probability < 26.1cm
+# II - probability < 26.1cm
 pnorm(26.1, mean = m, sd = sqrt(v), lower.tail = TRUE)
-
-# III
-# probability between 25 and 34
+# III - probability between 25 and 34
 pnorm(25, 34, mean = 28, sd = (16/sqrt(100)))
 pnorm(25, 34, mean = 28, sd = 4)
-
-# IV
-# sample size N=15, find distribution of sample mean
+# IV - sample size N=15, find distribution of sample mean
 N <- 15
 pnorm(N,m,(v/sqrt(N))) 
-
-# V
-# prob sample mean w/ N < 27cm
+# V - prob sample mean w/ N < 27cm
 N <- 27
 pnorm(N, mean = m, sd = sqrt(v), lower.tail = TRUE)
-
-# VI
-# what is D, such prob sample mean is 15 is less than D is 95%
+# VI - what is D, such prob sample mean is 15 is less than D is 95%
 N <- 15
 D <- m * v
 
@@ -42,6 +30,24 @@ N <- 38 # sample population of birds
 x <- 10.3 # sample mean
 s <- 4.1^2 # variance
 
-# i
-# construct a 95% confidence interval for population mean
-sigma = 4.1^2 # true population variance - z-test
+# I - construct a 95% confidence interval for population mean
+sigma <- 4.1^2 # true population variance using z-test
+sd <- sqrt(sigma)
+zval <- 1.96
+
+x - zval*(sd/sqrt(N))
+x + zval*(sd/sqrt(N))
+
+# II - p value for null hypothesis against alternative
+# H0: mu = 10 # population mean
+# Ha: mu > 10
+z <- (x - 10)/(sd/sqrt(N)) # 10 is H0 || z-test
+p <- pnorm(z, lower.tail = FALSE)
+
+# III 
+# H0: mu = 10.7
+# Ha: mu =/ 10.7
+z <- (x - 10.7)/(sd/sqrt(N))
+p <- 2*pnorm(z)
+
+# IV
