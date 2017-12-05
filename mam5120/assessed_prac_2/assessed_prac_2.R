c800806ceb97e511d546e815edec85e0cf9fdc8a
Samantha Pendleton <samanfapendle@outlook.com>
2017-12-05 17:54:27 +0000

    continued & added csv

--- mam5120/assessed_prac_2/assessed_prac_2.R
+++ mam5120/assessed_prac_2/assessed_prac_2.R
@@ -3,8 +3,8 @@
 #Question 1 - relationship between height (cm) & weight (kg) - 100 people
 
 #a - produce plot
-library(readr)
-weight_height <- read_csv("~/git/R/R/mam5120/assessed_prac_2/weight_height.csv")
+#library(readr)
+weight_height <- read.csv("~/git/R/R/mam5120/assessed_prac_2/weight_height.csv")
 #View(weight_height)
 attach(weight_height)
 plot(height, weight, main="Scatter Plot of Weight against Height of 100 People", ylab="Weight (kg)", xlab="Height (cm)")
@@ -17,11 +17,11 @@ hw.lm <-lm(weight~height)
 summary(hw.lm)
 a <- -45.73068
 b <- 0.67113
-equation <- "y <- a + (b*x)"
-ans <- "y <- -45.73068 + 0.67113(x)"
+equation <- "y <- a + bx"
+ans1b <- "y <- -45.73068 + 0.67113(x)"
 
 #c - what is meaning of Estimate in row labeled "height" in table of (b)
-ans <- "slope of the graph"
+ans1c <- "slope of the graph"
 
 ###################################################
 #Question 2 - effect of changes in pH on efficiency of an industrial process
@@ -31,28 +31,57 @@ eff <- c(30.2, 59, 37.3, 38, 42, 52, 56.3, 55.4, 43.3, 27.8, 44.8, 55.1)
 #a - best fitting models
 pe.lm <- lm(eff~ph)
 summary(pe.lm)
-m1.r2 <- 0.9808
 model1 <- "y <- -28.3889 + 10.8604(x)"
 pe.lm2 <- lm(eff~ph+(I(ph^2)))
 summary(pe.lm2)
-m2.r2 <- 0.987
 model2 <- "y <- 16.0767 - 2.9898(x) + 1.0562(x)"
-ans <- "model 2 better fit as R-squared is higher"
+ans2a <- "model 2 better fit as R-squared is higher"
 
 #b - proportion of variability is explained by each model
-summary(pe.lm)
 m1.r2 <- 0.9808
-m2.r2adj <- 0.9789
-summary(pe.lm2)
 m2.r2 <- 0.987
-m2.r2adj <- 0.9841
 
-#c - anova on model 2: whether inclusion of quadratic term significantly improves the fit of the model
+#c - anova
+#model 2: whether inclusion of quadratic term significantly improves the fit of the model
 #be explicit about exactly which null hypothesis the F-test is testing
-anova(lm(eff~ph)) #summary(aov(pe.lm))
+h0 <- "no significant difference"
+ha <- "some significant difference"
+anova(pe.lm, pe.lm2)
+p-value <- 0.06905
+ans2c <- "no need to reject null hypothesis as p-value (at 10%) is higher than 5%"
 
 ###################################################
 #Question 3
+Question3_Week9 <- read.csv("~/git/R/R/mam5120/assessed_prac_2/Question3_Week9.csv")
+#View(Question3_Week9)
+attach(Question3_Week9)
+A.df <- read.csv("Question3_Week9.csv")
+
+#a - fit linear model on y against x - generate standard diagnostic plots
+a.lm <- lm(y~x, data = A.df)
+summary(a.lm)
+plot(a.lm) #residuals vs fitted, normal Q-Q
+
+#b - log diagnostic plots
+alog.lm <- lm(log(y)~x, data = A.df)
+plot(alog.lm) #residuals vs fitted, normal Q-Q
+
+#c - fitted model in log form
+ans3c <- "log(y) <- a + bx"
+
+#d - what is corresponding interpretation of e
+ans3d <- "each time x is increased by 1, y is multiplied by e(^^bx)"
 
 ###################################################
-#Question 4
+#Question 4 - ANCOVA (analysis of covariance): one continious explanatory variable and one categorical explanatory variable (factor)
+#fit separate regression line for each value of the factor: may or may not be parallel
+#a
+#b
+
+#c - fit linear model regressing call rate (y) on temp (x)
+BirdCalls <- read.csv("BirdCalls.csv")
+plot(BirdCalls$Temp, BirdCalls$CallRate)
+
+bc.lm <- lm(CallRate~Temp, data = BirdCalls)
+summary(bc.lm)
+plot(bc.lm)
