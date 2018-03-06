233f92efa41aa8e20fa5f854c76db700c3d1643f
Samantha Pendleton <samanfapendle@outlook.com>
2018-03-06 11:37:28 +0000

    started workbook 2

---
+++ mam5220/w2/workbook2.R
@@ -0,0 +1,14 @@
+# MAM5220 - Statistical Techniques for Computational Biology (2017-18)
+# Workbook 2 - Time Series
+# Samantha Pendleton - sap21
+
+install.packages("TSA")
+library("TSA")
+
+# Question 1
+
+data(hare)
+plot(hare,ylab='Abundance', xlab='Year',type='o')
+
+##############################################################
+##############################################################
