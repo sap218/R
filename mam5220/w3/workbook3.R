376ffa050aefea31765d9083235e868d70f740a1
Samantha Pendleton <samanfapendle@outlook.com>
2018-04-29 16:15:40 +0100

    Started 2! about a quarter of the way yayyyyyyy

--- mam5220/w3/workbook3.R
+++ mam5220/w3/workbook3.R
@@ -4,6 +4,9 @@
 
 library(readr)
 
+##################################################################################
+
+# Part 1
 # Question 1
 crab <- read_csv("~/git/R/R/mam5220/w3/Crab_transcriptomics.csv")
 
@@ -71,10 +74,49 @@ write.csv(fold.change, "~/git/R/R/mam5220/w3/crab_subset_fold_change.csv")
 ##################################################################################
 ##################################################################################
 
+# Part 2
 LR <- read_csv("~/git/R/R/mam5220/w3/LR.transcriptomics.csv")
 dim(LR) # finding out size of dataframe - 5000 rows, 73 columns
 head(LR) # first six rows
 
+##################################################################################
+
 # Question 2
+nrep <- rep(4,18)
+times <- c(0,seq(6,54,by=3))
+times.vector <- rep(times,nrep)
+
+# a)
+par(mfrow=c(2,2))
+which(LR$X1 == "AT1G47830")
+AT1G47830 <- LR[2986,2:73]
+which(LR$X1 == "AT1G08940")
+AT1G08940 <- LR[662,2:73]
+which(LR$X1 == "AT1G01120")
+AT1G01120 <- LR[11,2:73]
+which(LR$X1 == "AT1G59840")
+AT1G59840 <- LR[3768,2:73]
+plot(times.vector, AT1G47830, xlab="Time", ylab="Log mRNA Levels", main="AT1G47830")
+plot(times.vector, AT1G08940, xlab="Time", ylab="Log mRNA Levels", main="AT1G08940")
+plot(times.vector, AT1G01120, xlab="Time", ylab="Log mRNA Levels", main="AT1G01120")
+plot(times.vector, AT1G59840, xlab="Time", ylab="Log mRNA Levels", main="AT1G59840")
+
+# b)
+# calculates the p-value associated with carrying out a one-way analysis of variance (ANOVA)
+# for a particular transcript using time as a factor
+
+oneway.ANOVA.func <- function(transcriptomics.data, times.vector, rowID){
+  data.vector <- as.numeric(transcriptomics.data[rowID,])
+  temp.aov <- aov(lm(data.vector~as.factor(times.vector)))
+  p.value <- summary(temp.aov)[[1]][1,5]
+}
+
+# use w/ for loop to calulate p-values for all 5000 genes
+# identify gene name for 6 most significant p-values
+# 2x3 plots of time profiles for these 6 genes
+
+# c)
+
+##################################################################################
 
 # Question 3
